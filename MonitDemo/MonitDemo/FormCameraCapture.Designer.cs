85d024c670eb3ab90180c5a16f309c8b424ad8b0
mcyyun <466039741@qq.com>
2017-12-15 14:23:54 +0800

    创建视频拍照AForge项目

---
+++ MonitDemo/MonitDemo/FormCameraCapture.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace MonitDemo
+{
+    partial class FormCameraCapture
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.videoSourcePlayer1 = new AForge.Controls.VideoSourcePlayer();
+            this.btnStart = new DevExpress.XtraEditors.SimpleButton();
+            this.btnStop = new DevExpress.XtraEditors.SimpleButton();
+            this.lupSetup = new DevExpress.XtraEditors.LookUpEdit();
+            this.btnStartRecord = new DevExpress.XtraEditors.SimpleButton();
+            this.btnStopRecord = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.lupSetup.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // videoSourcePlayer1
+            // 
+            this.videoSourcePlayer1.Location = new System.Drawing.Point(6, 55);
+            this.videoSourcePlayer1.Name = "videoSourcePlayer1";
+            this.videoSourcePlayer1.Size = new System.Drawing.Size(1005, 567);
+            this.videoSourcePlayer1.TabIndex = 0;
+            this.videoSourcePlayer1.Text = "videoSourcePlayer1";
+            this.videoSourcePlayer1.VideoSource = null;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(32, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "开始";
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(124, 12);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "结束";
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lupSetup
+            // 
+            this.lupSetup.Location = new System.Drawing.Point(242, 12);
+            this.lupSetup.Name = "lupSetup";
+            this.lupSetup.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.lupSetup.Size = new System.Drawing.Size(312, 20);
+            this.lupSetup.TabIndex = 5;
+            // 
+            // btnStartRecord
+            // 
+            this.btnStartRecord.Location = new System.Drawing.Point(587, 15);
+            this.btnStartRecord.Name = "btnStartRecord";
+            this.btnStartRecord.Size = new System.Drawing.Size(75, 23);
+            this.btnStartRecord.TabIndex = 6;
+            this.btnStartRecord.Text = "开始录制";
+            this.btnStartRecord.Click += new System.EventHandler(this.btnStartRecord_Click);
+            // 
+            // btnStopRecord
+            // 
+            this.btnStopRecord.Location = new System.Drawing.Point(698, 15);
+            this.btnStopRecord.Name = "btnStopRecord";
+            this.btnStopRecord.Size = new System.Drawing.Size(75, 23);
+            this.btnStopRecord.TabIndex = 7;
+            this.btnStopRecord.Text = "结束录制";
+            this.btnStopRecord.Click += new System.EventHandler(this.btnStopRecord_Click);
+            // 
+            // FormCameraCapture
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1013, 624);
+            this.Controls.Add(this.btnStopRecord);
+            this.Controls.Add(this.btnStartRecord);
+            this.Controls.Add(this.lupSetup);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.videoSourcePlayer1);
+            this.Name = "FormCameraCapture";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.lupSetup.Properties)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer1;
+        private DevExpress.XtraEditors.SimpleButton btnStart;
+        private DevExpress.XtraEditors.SimpleButton btnStop;
+        private DevExpress.XtraEditors.LookUpEdit lupSetup;
+        private DevExpress.XtraEditors.SimpleButton btnStartRecord;
+        private DevExpress.XtraEditors.SimpleButton btnStopRecord;
+    }
+}
+
---
+++ MonitDemo/MonitDemo/FormCameraCapture.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Video.FFMPEG;
+using DevExpress.XtraEditors.Controls;
+
+namespace MonitDemo
+{
+    public partial class FormCameraCapture : Form
+    {
+        //用来操作摄像头
+        private readonly VideoCaptureDevice _camera = null;
+        //用来把每一帧图像编码到视频文件
+        private readonly VideoFileWriter _videoOutPut = new VideoFileWriter();
+
+        private Bitmap bmp = new Bitmap(1, 1);
+
+        public FormCameraCapture()
+        {
+            InitializeComponent();
+
+            var devs = new FilterInfoCollection(FilterCategory.VideoInputDevice); //获取摄像头列表
+            _camera = new VideoCaptureDevice(devs[0].MonikerString); //实例化设备控制类(我选了第1个)
+
+            var cameraSetupList = new List<CameraSetup>();
+            int index = 0;
+            foreach (var resolution in _camera.VideoCapabilities)
+            {
+                var cameraSetup = new CameraSetup
+                    {
+                        AverageFrameRate = resolution.AverageFrameRate.ToString(),
+                        BitCount = resolution.BitCount.ToString(),
+                        MaximumFrameRate = resolution.MaximumFrameRate.ToString(),
+                        Height = resolution.FrameSize.Height.ToString(),
+                        Width = resolution.FrameSize.Width.ToString(),
+                        Index = index
+                    };
+                cameraSetup.Value = string.Format("{0}_{1}_{2}_{3}_{4}", cameraSetup.AverageFrameRate,
+                                                  cameraSetup.BitCount, cameraSetup.MaximumFrameRate, cameraSetup.Height,
+                                                  cameraSetup.Width);
+                cameraSetupList.Add(cameraSetup);
+                index++;
+            }
+            
+            lupSetup.Properties.DataSource = cameraSetupList;
+            lupSetup.Properties.Columns.Clear();
+            LookUpColumnInfoCollection coll = lupSetup.Properties.Columns;
+            coll.Add(new LookUpColumnInfo("AverageFrameRate", "AverageFrameRate", 0));
+            coll.Add(new LookUpColumnInfo("BitCount", "BitCount", 1));
+            coll.Add(new LookUpColumnInfo("MaximumFrameRate", "MaximumFrameRate", 2));
+            coll.Add(new LookUpColumnInfo("Height", "Height", 3));
+            coll.Add(new LookUpColumnInfo("Width", "Width", 4));
+            lupSetup.Properties.DisplayMember = "Value";
+            lupSetup.Properties.ValueMember = "Index";
+            lupSetup.Properties.BestFit();
+
+            btnStop.Enabled = false;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if(lupSetup.EditValue == null)
+                return;
+
+            btnStart.Enabled = false;
+            lupSetup.Enabled = false;
+            btnStop.Enabled = true;
+            _index = (int)lupSetup.EditValue;
+            //配置录像参数(宽,高,帧率,比特率等参数)VideoCapabilities这个属性会返回摄像头支持哪些配置,从这里面选一个赋值接即可,我选了第1个
+            _camera.VideoResolution = _camera.VideoCapabilities[_index];
+            //设置回调,aforge会不断从这个回调推出图像数据
+            _camera.NewFrame += Camera_NewFrame;
+
+            videoSourcePlayer1.VideoSource = _camera;
+            videoSourcePlayer1.Start();
+            _camera.Start();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = true;
+            lupSetup.Enabled = true;
+            btnStop.Enabled = false;
+            if (_camera != null)
+            {
+                _camera.Stop();
+                //关闭录像文件,如果忘了不关闭,将会得到一个损坏的文件,无法播放
+                _videoOutPut.Close();
+                videoSourcePlayer1.Stop();
+            }
+        }
+
+        private int _index = 1;
+        private void btnStartRecord_Click(object sender, EventArgs e)
+        {
+            _videoOutPut.Open(string.Format("E:/VIDEO_{0}.MP4", _index),
+                 _camera.VideoResolution.FrameSize.Width,
+                 _camera.VideoResolution.FrameSize.Height,
+                 _camera.VideoResolution.AverageFrameRate,
+                 VideoCodec.MPEG4,
+                 _camera.VideoResolution.BitCount);
+        }
+
+        private void btnStopRecord_Click(object sender, EventArgs e)
+        {
+            _videoOutPut.Close();
+        }
+
+        private void Camera_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            _videoOutPut.WriteVideoFrame(eventArgs.Frame);
+            lock (bmp)
+            {
+                //释放上一个缓存
+                bmp.Dispose();
+                //保存一份缓存
+                bmp = eventArgs.Frame.Clone() as Bitmap;
+            }
+        }
+    }
+
+    public class CameraSetup
+    {
+        public string AverageFrameRate { get; set; }
+        public string BitCount { get; set; }
+        public string MaximumFrameRate { get; set; }
+        public string Height { get; set; }
+        public string Width { get; set; }
+        public string Value { get; set; }
+        public int Index { get; set; }
+    }
+}
